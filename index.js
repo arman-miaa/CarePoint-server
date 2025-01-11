c7f8ab39f064d2d21b6a40698e3407a7b3d76357
Arman <mdarman365506@gmail.com>
2025-01-11 11:51:44 +0600

    removed verifyToken from details api

--- index.js
+++ index.js
@@ -64,77 +64,80 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const volunteerCollection = client.db('volunteerDB').collection('volunteer_posts');
-    const RequestCollection = client.db('volunteerDB').collection('volunteerRequests');
+    const volunteerCollection = client
+      .db("volunteerDB")
+      .collection("volunteer_posts");
+    const RequestCollection = client
+      .db("volunteerDB")
+      .collection("volunteerRequests");
 
-    
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-
     // auth related apis for jwt
-    
-    app.post('/jwt', (req, res) => {
+
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-     
+
       const token = jwt.sign(user, process.env.JWT_SECRET, {
-        expiresIn:'5h'
+        expiresIn: "5h",
       });
-      res.cookie("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      })
-      .send({success: true})
-    })
-
-
-
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     // get api
     app.get("/volunteerPosts", async (req, res) => {
-      const result = await volunteerCollection.find().limit(6).sort({postDeadline: 1}).toArray()
+      const result = await volunteerCollection
+        .find()
+        .limit(8)
+        .sort({ postDeadline: 1 })
+        .toArray();
       res.send(result);
     });
 
     // get all post
-    app.get('/allPost',  async (req, res) => {
-      const search = req.query.search || '';
-    
-      const result = await volunteerCollection.find({title:{$regex: search, $options: 'i'}}).toArray();
-      res.send(result);
-    })
+    app.get("/allPost", async (req, res) => {
+      const search = req.query.search || "";
 
+      const result = await volunteerCollection
+        .find({ title: { $regex: search, $options: "i" } })
+        .toArray();
+      res.send(result);
+    });
 
     // get data by dynamic id
-    app.get("/volunteerPosts/:id", verifyToken, async (req, res) => {
+    app.get("/volunteerPosts/:id",  async (req, res) => {
       const id = req.params.id;
-     
+
       const query = { _id: new ObjectId(id) };
       const result = await volunteerCollection.findOne(query);
       res.send(result);
     });
 
-
     // get my post
 
-app.get("/myPost/:email", verifyToken, async (req, res) => {
-  const email = req.params.email;
-  // console.log("Email from params:", email);
-  // console.log("Token from cookies:", req.cookies?.token);
-  // console.log("Decoded user:", req.user);
-  if (req.user.email !== email) {
-    return res.status(403).send({ message: "Forbidden access" });
-  }
-  const query = { organizerEmail: email };
-  const result = await volunteerCollection.find(query).toArray();
-  res.send(result);
-});
-
+    app.get("/myPost/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      // console.log("Email from params:", email);
+      // console.log("Token from cookies:", req.cookies?.token);
+      // console.log("Decoded user:", req.user);
+      if (req.user.email !== email) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      const query = { organizerEmail: email };
+      const result = await volunteerCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // get request post to be a volunteer
-    app.get('/myRequestPost/:email', verifyToken, async (req, res) => {
+    app.get("/myRequestPost/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
       // console.log("Email from params:", email);
       // console.log("Token from cookies:", req.cookies?.token);
@@ -145,31 +148,28 @@ app.get("/myPost/:email", verifyToken, async (req, res) => {
       const query = { volunteerEmail: email };
       const result = await RequestCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
     // remove token from cookie if logged out user
-    app.post('/logout', (req, res) => {
+    app.post("/logout", (req, res) => {
       res
         .clearCookie("token", {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
-          sameSite:
-          process.env.NODE_ENV === "production" ? "none" : "strict",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
-      .send({success: true})
-    })
+        .send({ success: true });
+    });
 
-
-    app.post('/volunteerPosts',verifyToken,  async (req, res) => {
+    app.post("/volunteerPosts", verifyToken, async (req, res) => {
       const newVolunteerPost = req.body;
       // console.log(newVolunteerPost);
-      const result = await volunteerCollection.insertOne(newVolunteerPost)
+      const result = await volunteerCollection.insertOne(newVolunteerPost);
       res.send(result);
-    })
-
+    });
 
     // for request collection APIs
-    
+
     app.post("/volunteerRequests", verifyToken, async (req, res) => {
       const newRequest = req.body;
       const { postId } = newRequest;
@@ -189,55 +189,58 @@ app.get("/myPost/:email", verifyToken, async (req, res) => {
       });
     });
 
-
     // update api
 
-    app.patch('/updatePost/:id', verifyToken, async (req, res) => {
-
+    app.patch("/updatePost/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const updateData = req.body;
-         const email = req.user.email;
-        //  console.log('post update',email);
-         if (req.user.email !== email) {
-           return res.status(403).send({ message: "Forbidden access" });
-         }
+      const email = req.user.email;
+      //  console.log('post update',email);
+      if (req.user.email !== email) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
 
-      const result = await volunteerCollection.updateOne({ _id: new ObjectId(id) }, { $set: updateData });
+      const result = await volunteerCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData }
+      );
       res.send(result);
-
-    })
+    });
 
     // delete post by id
-    app.delete('/deletePost/:id', verifyToken, async(req, res) => {
+    app.delete("/deletePost/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const email = req.user.email;
       // console.log('post delete',email);
-       if (req.user.email !== email) {
-         return res.status(403).send({ message: "Forbidden access" });
-       }
-      
-      const result = await volunteerCollection.deleteOne({ _id: new ObjectId(id) });
+      if (req.user.email !== email) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+
+      const result = await volunteerCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
       res.send(result);
-    })
-app.get("/checkToken", verifyToken, (req, res) => {
-  res.status(200).send({ message: "Token is valid" });
-});
+    });
+    app.get("/checkToken", verifyToken, (req, res) => {
+      res.status(200).send({ message: "Token is valid" });
+    });
 
-
-    // delete post by id for request 
+    // delete post by id for request
     app.delete('/deleteRequest/:id', verifyToken, async(req, res) => {
       const id = req.params.id;
       const email = req.user.email;
       // console.log("post delete request", email);
+      // const { postId } = req.body;
 
        if (req.user.email !== email) {
          return res.status(403).send({ message: "Forbidden access" });
-       }
-     
-      
+      }
+
       const result = await RequestCollection.deleteOne({ _id: new ObjectId(id) });
       res.send(result);
     })
+
+  
 
 
   } finally {
